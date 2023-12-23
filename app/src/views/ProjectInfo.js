9edf5ab9de8f9629394e3cc5d358400e60ffbb39
Erez Lev <erezl@MacBook-Pro.lan>
2023-12-23 10:33:43 +0200

    fix changes

--- app/src/views/ProjectInfo.js
+++ app/src/views/ProjectInfo.js
@@ -1,28 +1,37 @@
-import React, { useRef } from 'react';
-import '../styles/Projects/ProjectInfo.css';
-import { CSSTransition } from 'react-transition-group';
-import '../styles/transitions/transitions.css';
+import React from "react";
+import "../styles/Projects/ProjectInfo.css";
+import { CSSTransition } from "react-transition-group";
+import "../styles/transitions/transitions.css";
 
 const ProjectInfo = ({ projectName, sqm, location, architect, show }) => {
-
   return (
-    <CSSTransition in={show} classNames="slide-fade" timeout={1000} unmountOnExit>
+    <CSSTransition
+      in={show}
+      classNames="slide-fade"
+      timeout={1000}
+      unmountOnExit
+    >
       <div className="side-panel">
-          <div className="project-name" style={{ fontSize: "17px", color: "#686868" }}>{projectName}</div>
-          <div className="property-info">
-            <div className="info-item">
-              <div className="label">Sqm</div>
-              <div className="value">{sqm}</div>
-            </div>
-            <div className="info-item">
-              <div className="label">Location</div>
-              <div className="value">{location}</div>
-            </div>
-            <div className="info-item">
-              <div className="label">Architect</div>
-              <div className="value">{architect}</div>
-            </div>
+        <div
+          className="project-name"
+          style={{ fontSize: "17px", color: "#686868" }}
+        >
+          {projectName}
+        </div>
+        <div className="property-info">
+          <div className="info-item">
+            <div className="label">Sqm</div>
+            <div className="value">{sqm}</div>
           </div>
+          <div className="info-item">
+            <div className="label">Location</div>
+            <div className="value">{location}</div>
+          </div>
+          <div className="info-item">
+            <div className="label">Architect</div>
+            <div className="value">{architect}</div>
+          </div>
+        </div>
       </div>
     </CSSTransition>
   );
