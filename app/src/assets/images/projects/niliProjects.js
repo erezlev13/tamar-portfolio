3002d8fb173e493fa79145a669e84beed2329022
Erez Lev <erezl@MacBook-Pro.lan>
2023-12-23 15:26:00 +0200

    add bareli project and about

--- app/src/assets/images/projects/niliProjects.js
+++ app/src/assets/images/projects/niliProjects.js
@@ -1,4 +1,39 @@
 import photo1 from './nili/bareli_01.jpeg';
 import photo2 from "./nili/bareli_02.jpg";
+import photo3 from './nili/Bareli001.jpg';
+import photo4 from "./nili/Bareli_01.jpg";
+import photo5 from "./nili/Bareli__02.jpg";
+import photo6 from "./nili/Bareli_03.jpg";
+import photo7 from "./nili/Bareli_04.jpg";
+import photo8 from "./nili/Bareli_05.jpg";
+import photo9 from "./nili/Bareli_06.jpg";
+import photo10 from "./nili/Bareli_07.jpg";
+import photo11 from "./nili/Bareli_08.jpg";
+import photo12 from "./nili/Bareli_09.jpg";
+import photo13 from "./nili/Bareli_10.jpg";
+import photo14 from "./nili/Bareli_11.jpg";
+import photo15 from "./nili/Bareli12.jpg";
+import photo16 from "./nili/Bareli13.jpg";
+import photo17 from "./nili/Bareli_08.jpg";
+import photo18 from "./nili/Bareli_14.jpg";
+import photo19 from "./nili/Bareli22.jpg";
 
-export const NiliImages = [photo1, photo2];
+export const NiliImages = [
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
+  photo19,
+];
--- app/src/pages/AboutPage.js
+++ app/src/pages/AboutPage.js
@@ -1,83 +1,51 @@
-import React from 'react';
+import React from "react";
 
 import {
-	TextBox,
-	ImageBox,
-	Image,
-	Text,
-	AboutContainer,
-} from '../styles/About/About';
-import tamarProfile from '../assets/images/home/Tamar_Profile_Smile.jpeg';
+  TextBox,
+  ImageBox,
+  Image,
+  Text,
+  AboutContainer,
+} from "../styles/About/About";
+import tamarProfile from "../assets/images/home/Tamar_Profile_Smile.jpeg";
 
 const AboutPage = () => {
-	return (
-		<>
-			<AboutContainer>
-				<ImageBox>
-					<Image src={tamarProfile} />
-				</ImageBox>
-				<TextBox>
-					<Text>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Donec posuere nisi eu hendrerit pharetra. Nunc lacinia
-						nunc a finibus vehicula. Cras non ipsum gravida,
-						fringilla arcu eget, condimentum eros. Etiam viverra
-						euismod ultricies. Proin commodo a lacus vitae
-						efficitur. Vivamus tristique urna et ipsum malesuada
-						convallis. Proin at euismod nunc, ut condimentum lacus.
-						Proin at nisl eu velit aliquet vulputate. Integer
-						rhoncus metus quam, ac accumsan mauris imperdiet ac.
-						Donec maximus elit et augue dictum aliquet. Proin
-						consequat nibh eu felis porta, a aliquam lacus
-						imperdiet. Ut in placerat leo. Nam pretium pharetra ex,
-						non auctor sem ornare sit amet. Vestibulum purus odio,
-						dictum a tristique non, fermentum in quam. Maecenas et
-						massa velit. Suspendisse potenti. Integer nulla ipsum,
-						mollis quis eleifend vitae, rutrum et augue. Nam
-						accumsan lacus nec augue aliquet scelerisque. Ut at
-						cursus sapien. Suspendisse in lorem ante. Vivamus
-						imperdiet orci in dui malesuada lobortis. Pellentesque
-						ullamcorper risus sed porta vestibulum. Mauris non
-						vehicula justo. Cras aliquet mauris quis augue auctor
-						scelerisque. Phasellus rutrum, nunc quis tincidunt
-						euismod, diam elit pharetra leo, sit amet tempor dolor
-						libero et purus. Praesent mollis tempor lacus ut
-						eleifend. Etiam commodo lorem et justo egestas, eu
-						varius urna faucibus. Integer iaculis, ante vitae dictum
-						egestas, purus libero mattis ex, vel ultrices massa elit
-						vel nisi. Nulla malesuada faucibus aliquet. Orci varius
-						natoque penatibus et magnis dis parturient montes,
-						nascetur ridiculus mus. Proin dignissim orci sit amet
-						purus hendrerit faucibus. Donec sollicitudin tortor in
-						nunc porttitor mattis. Phasellus pharetra tincidunt
-						odio, et dictum augue venenatis et. Sed rhoncus, massa
-						id finibus viverra, leo est commodo augue, sed porttitor
-						ex arcu viverra enim. Donec et semper nisl. Proin eget
-						sem ipsum. Phasellus molestie tellus ligula, sit amet
-						rutrum lectus tincidunt ut. Quisque eleifend ornare
-						porttitor. Phasellus dapibus nunc ut lorem pulvinar, et
-						ultricies diam commodo. Sed in lectus ut lectus posuere
-						mollis sed sed dolor. Donec rutrum est ut lacus
-						venenatis, non elementum augue mattis. Nullam justo
-						dolor, rhoncus sed est commodo, egestas tempor elit.
-						Vestibulum sagittis diam magna, eu volutpat lacus
-						facilisis sed. Sed vehicula vulputate lectus, vel
-						fringilla velit pellentesque at. Vestibulum aliquam odio
-						sit amet iaculis lobortis. Quisque lobortis sem id metus
-						mattis, eu hendrerit urna porttitor. Integer id arcu a
-						lectus vestibulum rhoncus. Nullam eu fermentum ex, id
-						cursus dolor. Aliquam et finibus tortor, non maximus
-						odio. Donec semper congue commodo. Pellentesque pharetra
-						ipsum velit, eu gravida dolor hendrerit vel. Donec
-						consectetur felis ut condimentum eleifend. Vivamus
-						pulvinar quam sit amet efficitur pellentesque. Donec mi
-						purus, eleifend id odio et, egestas bibendum nisl.
-						Integer
-					</Text>
-				</TextBox>
-			</AboutContainer>
-		</>
-	);
+  return (
+    <>
+      <AboutContainer>
+        <ImageBox>
+          <Image src={tamarProfile} />
+        </ImageBox>
+        <TextBox>
+          <Text>
+            Tamar Lev’s studio is situated in the heart of Tel Aviv. Having
+            contributed her expertise to esteemed establishments, including
+            Baranowitz & Goldberg, Pitsou Kedem in Israel, and UHA London in the
+            UK, she brings a multifaceted understanding of design cultivated
+            across diverse cultural landscapes.
+            <br />
+            <br />
+            Her experience spans diverse projects encompassing commercial and
+            residential spaces on various scales, from high-end residences to
+            restaurants, offices, showrooms, and spas. The studio is dedicated
+            to exploring new materials, textures, and innovative combinations,
+            ensuring that each design bears meticulous attention to detail and a
+            pursuit of excellence.
+            <br />
+            <br />
+            Tamar’s studio crafts minimalist spaces that resonate with purpose,
+            elegance, and functionality, pushing architectural boundaries
+            through a unique blend of experience and creative vision.
+            <br />
+            <br />
+            Every project is unique, tailored to the client’s needs and
+            environment, requiring a customized design and a distinctive story
+            to be told.
+          </Text>
+        </TextBox>
+      </AboutContainer>
+    </>
+  );
 };
 
 export default AboutPage;
